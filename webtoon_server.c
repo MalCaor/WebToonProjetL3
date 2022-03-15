2dd4ac9811e7c199fd902657d4f8e871028ae733
Narrateur <theo.paste@outlook.fr>
2022-03-15 08:26:39 +0100

    server init

--- webtoon_server.c
+++ webtoon_server.c
@@ -14,9 +14,16 @@ init_1_svc(void *argp, struct svc_req *rqstp)
 {
 	static char * result;
 
-	/*
-	 * insert server code here
-	 */
+	struct serie s1;
+	strcpy(s1.titre, "Comment je suis devenu un hero alors que j'étais un looser");
+	s1.noteMoyenne;
+	strcpy(s1.listGenre[0].nomGenre,"Humour");strcpy(s1.listGenre[0].nomGenre,"Action");
+	s1.dateSerie.jour=10; s1.dateSerie.mois=1; s1.dateSerie.annee=2020;
+	strcpy(s1.listComm[0].comm,"Sympa"); strcpy(s1.listComm[0].pseudo,"Pseudo 1"); 
+	strcpy(s1.listComm[1].comm,"Bof deja vu"); strcpy(s1.listComm[1].pseudo,"Pseudo 2"); 
+	s1.nbrVue=10;
+	s1.nbEpisode=3;
+	strcpy(s1.description, "Une aventure incroyable dans un monde parellele apres avoir été renversé par un tricycle");
 
 	return (void *) &result;
 }
